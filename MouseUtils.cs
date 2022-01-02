c71a0e0feb41f70d361e0869b292d0ce541acc79
Ernakh <fabriciotonettolondero@gmail.com>
2022-01-02 09:22:56 -0300

    first commit

---
+++ MouseUtils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace AutoClickTimer
+{
+    internal class MouseUtils
+    {
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        [DllImport("user32.dll")]
+        public static extern int SetCursorPos(int x, int y);//não estou usando
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+
+        public void click()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+
+            int x = lpPoint.X;
+            int y = lpPoint.Y;
+
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// O programa clica a cada 2 minutos na posição que o mouse estiver
+
+
+
+using AutoClickTimer;
+
+Console.WriteLine("O programa clica a cada 2 minutos na posição que o mouse estiver...");
+
+
+while (true)
+{
+    Console.WriteLine("Aguarde 2 minutos!");
+
+    Thread.Sleep(120000);
+
+    MouseUtils mouse = new MouseUtils();
+
+    mouse.click();
+
+    Console.WriteLine("Clicado!");
+}
